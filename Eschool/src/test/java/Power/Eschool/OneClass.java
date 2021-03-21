e775ca3dac17dec60590e5f3e4b87ba666c4898b
Sambit <sambit4545@gmail.com>
2021-03-21 09:09:26 +0530

    Commit by person one file is OneClass

---
+++ Eschool/src/test/java/Power/Eschool/OneClass.java
@@ -0,0 +1,14 @@
+package Power.Eschool;
+
+public class OneClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello from one class. Person one");
+		System.out.println("Hello from one class. Person one");
+		System.out.println("Hello from one class. Person one");
+		System.out.println("Hello from one class. Person one");
+
+	}
+
+}
