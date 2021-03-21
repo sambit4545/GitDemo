1d900fd8dd446dd566f883dbc539276d3f0ad72b
Sambit <sambit4545@gmail.com>
2021-03-21 10:14:47 +0530

    New class added

---
+++ Eschool/src/test/java/Power/Eschool/NewClass.java
@@ -0,0 +1,10 @@
+package Power.Eschool;
+
+public class NewClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello from new class.");
+	}
+
+}
